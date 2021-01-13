ad5d7902a880c741e8c1751832e0ce03d51a047a
behnaz rafiei <Behnaz.Rafiei@gmail.com>
2021-01-13 15:16:38 +0330

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,5 @@ $('.navbar').on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
 
     $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 500);
+        scrollTop: $($.attr(this, 'href')).offset().top}, 500);
 });
